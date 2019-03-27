68a5b03f807b620fed48275c80a3874764d98b6e
meredaniprogrammer <meredanielugochukwu@gmail.com>
2019-03-27 11:09:50 +0000

    My function Program

---
+++ function-101.js
@@ -0,0 +1,12 @@
+//Javaceipt function
+/* A function definition (also called a function declaration, or function statement) consists of the function keyword, followed by:
+
+    The name of the function.
+    A list of parameters to the function, enclosed in parentheses and separated by commas.
+    The JavaScript statements that define the function, enclosed in curly brackets, { }.
+*/
+
+let myFunction = function(num){
+    console.log(num * num)
+}
+myFunction(5)
