57137d5350afbd7faa6bb96bd270da82fcef1b9d
meredaniprogrammer <meredanielugochukwu@gmail.com>
2019-03-28 14:23:22 +0000

    Object in Javascript

---
+++ object-002.js
@@ -0,0 +1,9 @@
+//Object with template strings
+let myProfile = {
+    surname: 'Charles',
+    firstname: 'Soludo',
+    age: 27,
+    city: 'Abuja'
+}
+
+console.log(`His name is ${myProfile.surname} and his other name is ${myProfile.firstname}. He is ${myProfile.age} years old, he stays in ${myProfile.city}`)
