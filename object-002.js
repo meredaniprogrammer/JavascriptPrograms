b6dfcc23b627d05242b0d45c0eb5d790939795c6
meredaniprogrammer <meredanielugochukwu@gmail.com>
2019-03-28 14:32:58 +0000

    Updated Object in Javascript

--- object-002.js
+++ object-002.js
@@ -6,4 +6,10 @@ let myProfile = {
     city: 'Abuja'
 }
 
+//console.log(`His name is ${myProfile.surname} and his other name is ${myProfile.firstname}. He is ${myProfile.age} years old, he stays in ${myProfile.city}`)
+//We could use the dot notation the change the values
+
+myProfile.surname = 'Mere'
+myProfile.firstname = 'Daniel'
+myProfile.age = 23
 console.log(`His name is ${myProfile.surname} and his other name is ${myProfile.firstname}. He is ${myProfile.age} years old, he stays in ${myProfile.city}`)
