413ef6ccade3e6da9826a5eeba5b102851bdec38
meredaniprogrammer <meredanielugochukwu@gmail.com>
2019-03-29 09:58:33 +0000

    Using this keyword

--- object-methods.js
+++ object-methods.js
@@ -14,3 +14,16 @@ let restaurant = {
 restaurant.checkAvailability(300)
 //We currently do not have seats available
 
+//Using the "this" keyword
+let restaurant = {
+    name: 'Mr Bigs',
+    guestCapacity: 275,
+    guestCount: 0,
+    checkAvailability: function(partSize){
+        console.log(this)
+        return true
+    }
+}
+let status = restaurant.checkAvailability(4)
+console.log(status)
+
