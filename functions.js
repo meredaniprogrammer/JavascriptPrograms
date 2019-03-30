924a96bc81a0ace933371af9456daae0253af4d2
meredaniprogrammer <meredanielugochukwu@gmail.com>
2019-03-30 11:24:49 +0000

    Updated functions statements

--- functions.js
+++ functions.js
@@ -11,3 +11,19 @@ function examFunction(name, score){
     }
 }
 examFunction('Mere Daniel', 95)
+
+// The function can called several times
+
+//Functions
+function examFunction(name, score){
+    let passUni = 80
+    let passCol = 70
+    if (score >= passUni){
+        console.log('Congratulations!' + name + ' you have just been enrolled into our university')
+    }else if(score >= passCol && score <= 79){
+        console.log('Congrats! ' + name +  ' you have been enrolled into our college')
+    }else{
+        console.log('Sorry, you can\'t be offered admission into any school')
+    }
+}
+examFunction('Mere Daniel', 95)
