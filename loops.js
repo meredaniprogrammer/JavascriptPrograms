c0822b48353ba821fa57d6c0bd3a29e2622bcf30
meredaniprogrammer <meredanielugochukwu@gmail.com>
2019-04-04 12:23:13 +0000

    Looping over Arrays

---
+++ loops.js
@@ -0,0 +1,10 @@
+//Loop over arrays
+
+let arr = ['New York', 'Brazil', 'Nigeria', 'South Africa', 'Portugal']
+
+for(i=0; i< 4; i++){
+    if(arr[i] === 'New York'){
+        console.log(arr[i] + ' is where am going to next')
+    }
+}
+//New York is where am going to next
