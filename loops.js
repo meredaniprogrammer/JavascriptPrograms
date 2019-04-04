30147f1a0fe84cbe401087e8405081c60af0e4c0
meredaniprogrammer <meredanielugochukwu@gmail.com>
2019-04-04 13:49:09 +0000

    Updated Loops

--- loops.js
+++ loops.js
@@ -8,3 +8,20 @@ for(i=0; i< 4; i++){
     }
 }
 //New York is where am going to next
+
+//while loop
+
+let array = ['New York', 'Brazil', 'Nigeria', 'South Africa', 'Portugal']
+
+let i = 0
+while(i < array.length){
+    console.log(array[i])
+    i++
+}
+/* 
+New York
+Brazil
+Nigeria
+South Africa
+Portugal
+*/
