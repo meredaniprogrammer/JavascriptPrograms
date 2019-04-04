cec2c89552aaa5994574afea75fe73879417b1d1
meredaniprogrammer <meredanielugochukwu@gmail.com>
2019-04-04 10:16:53 +0000

    Updated Array Methods

--- object-methods3.js
+++ object-methods3.js
@@ -13,3 +13,21 @@ let father = {
 console.log(father.myFunct(father.daughter.name, father.name, father.daughter.age))
 
 //Bernadine is the daughter of Mere Daniel . She is 5 years old.
+
+let profilePerson = {
+    name: 'Mere Daniel',
+    prof: 'Student',
+    regNo: 'MOUAU/CME/14/12542',
+    dept: 'Computer Engineering',
+    son: {
+        name: 'Mere Wonder',
+        age: 5,
+        city: 'New York',
+        interest: 'Football',
+    },
+    myFunct: function(father, school, number, boy, age, sport){
+        return 'father,' + ' a ' + school + ' with reg no ' + number + ' has a son named ' + boy + ' who plays ' + sport 
+    }
+}
+let summary = profilePerson.myFunct(profilePerson.name, profilePerson.dept, profilePerson.regNo, profilePerson.son.name, profilePerson.son.interest)
+console.log(summary)
