2631fa74e0fdf14bdb0ac4666993d9f022bd533d
meredaniprogrammer <meredanielugochukwu@gmail.com>
2019-04-16 12:31:36 +0100

    Updated Looping over Arrays

--- loopingArrays.js
+++ loopingArrays.js
@@ -15,3 +15,24 @@ notes.forEach(function(item, index){
 5. Read
 6. Code
 */
+
+//looping over arrays with  forloop
+
+let notes = ['Eat', 'Jump', 'Run', 'Visit Libry', 'Read', 'Code']
+
+for(let count = 0; count < notes.length; count ++){
+    let num = count + 1
+    let note = notes[count]
+    console.log(`${num}. ${note}`)
+}
+
+/*
+
+1. Eat
+2. Jump
+3. Run
+4. Visit Libry
+5. Read
+6. Code
+
+*/
