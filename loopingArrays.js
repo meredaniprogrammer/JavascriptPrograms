5f32ae1201ede690e790a4896bb2bcd2baf7d3f0
meredaniprogrammer <meredanielugochukwu@gmail.com>
2019-04-16 11:24:07 +0100

    Looping Over Arrays

---
+++ loopingArrays.js
@@ -0,0 +1,17 @@
+//Looping over Arrays
+
+let notes = ['Eat', 'Jump', 'Run', 'Visit Libry', 'Read', 'Code']
+
+notes.forEach(function(item, index){
+    let num = index + 1
+    console.log(`${num}.` + ` ${item}`)
+})
+
+/*
+1. Eat
+2. Jump
+3. Run
+4. Visit Libry
+5. Read
+6. Code
+*/
